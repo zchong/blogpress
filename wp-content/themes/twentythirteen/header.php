5ffd20036075c1171f43fce9e0e5d0414eb29708
Zhia Chong <zhiachong@gmail.com>
2013-10-24 01:47:26 -0700

    Added a nice hover effect to header icon that will eventually lead back to home page. And removed big header from single post

--- wp-content/themes/twentythirteen/header.php
+++ wp-content/themes/twentythirteen/header.php
@@ -32,20 +32,25 @@
 
 <body <?php body_class(); ?>>
 	<div id="page" class="hfeed site">
-		<header id="masthead" class="site-header" role="banner">
+        <header id="masthead" class="site-header" role="banner">
             <div id="navbar" class="navbar">
                 <nav id="site-navigation" class="navigation main-navigation" role="navigation">
                     <h3 class="menu-toggle"><?php _e( 'Menu', 'twentythirteen' ); ?></h3>
                     <a class="screen-reader-text skip-link" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentythirteen' ); ?>"><?php _e( 'Skip to content', 'twentythirteen' ); ?></a>
-                    <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu head-image') ); ?>
+                    <a href='#' class='head-image'>Home</a><?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu') ); ?>
                     <?php get_search_form(); ?>
                 </nav><!-- #site-navigation -->
             </div><!-- #navbar -->
-            <div class="home-link" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+            <?php if ( !is_single() )
+            {
+            ?>
+                <div class="home-link" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
                 <h1 class="site-title"><a id="site-home-title" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
-                <!--<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2> -->
-            </div>
-
+            <!--<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2> -->
+    </div>
+            <?php } ?>
 		</header><!-- #masthead -->
-        <div id="intro" class="separator"></div>
+        <?php if ( !is_single() )
+                    echo '<div id="intro" class="separator"></div>';
+        ?>
 		<div id="main" class="site-main">
